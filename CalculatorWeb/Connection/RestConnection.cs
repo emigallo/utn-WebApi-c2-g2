0957569eb28b0454f239034d11a4f3e833908637
Fabián Martin Prezzavento <fabian.prezzavento@hotmail.com>
2022-02-06 14:07:26 -0300

    Actualización del CalculatorWeb, con lo último que se vio en clase.

---
+++ CalculatorWeb/Connection/RestConnection.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CalculatorWeb.Connection
+{
+    public class RestConnection
+    {
+        private HttpClient _httpClient;
+        private string _baseUri;
+
+        public RestConnection(string baseUri)
+        {
+            this._httpClient = new HttpClient();
+            this._baseUri = baseUri;
+        }
+
+        public async Task<T> GetAsync<T>(string partialUri)
+        {
+            // baseUri: http://localhost:5500
+            // partialUri: calc
+
+            string fullUri = string.Format("{0}/{1}", this._baseUri, partialUri);
+
+            HttpResponseMessage response = await this._httpClient.GetAsync(fullUri);
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception("Ocurrió un error al hacer el llamado a la API");
+            }
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+
+
+        //private List<UserModel> GetFromSql()
+        //{
+        //    SqlCommand sqlCommand = new SqlCommand(sqlConnection, "SELECT * FROM Users WHERE Id > 3");
+
+        //    SqlDataReader reader = sqlCommand.ExecuteReader();
+
+        //    List<UserModel> rett = new List<UserModel>();
+        //    while (reader.Read())
+        //    {
+        //        rett.Add(new UserModel()
+        //        {
+        //            Name = reader.GetString("Name"),
+        //            Email = reader.GetString("Email")
+        //        });
+        //    }
+
+        //     //rett.Where(x => x.Name == "Emiliano");
+        //    return rett;
+        //}
+    }
+}
--- CalculatorWeb/Controllers/HomeController.cs
+++ CalculatorWeb/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
 ﻿using CalculatorWeb.Models;
+using CalculatorWeb.Services;
+using CalculatorWeb.ViewModels.Home;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CalculatorWeb.Controllers
 {
@@ -32,6 +30,49 @@ namespace CalculatorWeb.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Create(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Calc");
+            }
+
+            ViewBag.Error = "Hay campos sin completar";
+            return View("Welcome", model);
+        }
+
+        [HttpGet]
+        [Route("Calc")]
+        public IActionResult Calc()
+        {
+            var model = new CalcViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("Calc")]
+        public IActionResult Calculate(CalcViewModel model, string lastInput)
+        {
+            var service = new CalculateModelService();
+
+            if (lastInput == "=")
+            {
+                model.Result = service.CalculateResult();
+            }
+
+            if (lastInput == "+")
+            {
+
+            }
+
+            if (int.TryParse(lastInput, out int number))
+            {
+
+            }
+
+            return View("Calc", model);
         }
     }
 }
