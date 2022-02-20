cabbabedaeb821a11960c970857c65bdcc349cba
Emanuel Jose Carrera <emcarrer@emeal.nttdata.com>
2022-02-20 17:00:15 -0300

    final

--- TicTacToeAPI/Business/Models/Board.cs
+++ TicTacToeAPI/Business/Models/Board.cs
@@ -10,7 +10,7 @@ namespace TicTacToeBusiness
             Positions = new int[9];
         }
 
-        private int[] Positions { get; set; }
+        public int[] Positions { get; set; }
 
         public GameStatus AddToken(Player player, int position)
         {
